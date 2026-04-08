46b08ba64d2fc9123da758ac7c4916d05e8dba7d
Munisa Nurboyeva <nurboyevaa@Munisas-MacBook.local>
2026-04-08 12:36:01 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const form = document.querySelector('form')
+const input = document.querySelector('.input')
+const cards = document.querySelector('.cards')
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+
+    countryFunc(input.value.toUpperCase());
+    input.value = ''
+}) 
+
+async function countryFunc(country) {
+    const res = await fetch(
+        `https://api.api-ninjas.com/v1/countryflag?country=${country}`,
+
+        {
+            headers: {
+                "X-Api-Key": "GnxpyjjORxM1tGWnAHb104SzKiIG8McqnS7opIpX"
+            }
+        }
+    )
+    const data = await res.json()
+    render(data, cards)
+}
+
+function render(data, parent){
+    parent.innerHTML = 
+    `
+        <div class="card">
+            <h3 class="title">${data.country}</h3>
+            <img src="${data.square_image_url}" class="flag" alt="...">
+        </div>
+    `
+}
